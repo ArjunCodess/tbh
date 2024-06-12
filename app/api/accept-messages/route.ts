d96b6113e655d4a03d5b60f164215e7558c037c2
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-06-12 19:17:57 +0530

    get-messages - GET & accept messages - GET and POST methods created.

---
+++ app/api/accept-messages/route.ts
@@ -0,0 +1,56 @@
+import UserModel from '@/app/lib/models/user.schema';
+import { User } from 'next-auth';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../auth/[...nextauth]/options';
+import connectToDatabase from '@/app/lib/connectToDatabase';
+
+export async function GET(request: Request) {
+     await connectToDatabase();
+
+     const session = await getServerSession(authOptions);
+     const user = session?.user;
+
+     if (!session || !user) {
+          return Response.json({ success: false, message: 'Not authenticated' }, { status: 401 });
+     }
+
+     try {
+          const foundUser = await UserModel.findById(user._id);
+
+          if (!foundUser) return Response.json({ success: false, message: 'User not found' }, { status: 404 });
+
+          return Response.json({ success: true, isAcceptingMessages: foundUser.isAcceptingMessages }, { status: 200 });
+     }
+
+     catch (error) {
+          console.error('Error retrieving message acceptance status: ', error);
+          return Response.json({ success: false, message: 'Error retrieving message acceptance status' }, { status: 500 });
+     }
+}
+
+export async function POST(request: Request) {
+     await connectToDatabase();
+
+     const session = await getServerSession(authOptions);
+
+     const user: User = session?.user;
+
+     if (!session || !session.user) return Response.json({ success: false, message: 'Not authenticated' }, { status: 401 });
+
+     const userId = user._id;
+
+     const { acceptMessages } = await request.json();
+
+     try {
+          const updatedUser = await UserModel.findByIdAndUpdate(userId, { isAcceptingMessages: acceptMessages }, { new: true });
+
+          if (!updatedUser) return Response.json({ success: false, message: 'Unable to find user to update message acceptance status' }, { status: 404 });
+
+          return Response.json({ success: true, message: 'Message acceptance status updated successfully', updatedUser }, { status: 200 });
+     }
+
+     catch (error: any) {
+          console.error('Error updating message acceptance status: ', error);
+          return Response.json({ success: false, message: 'Error updating message acceptance status' }, { status: 500 });
+     }
+}
---
+++ app/api/get-messages/route.ts
@@ -0,0 +1,45 @@
+import UserModel from '@/app/lib/models/user.schema';
+import mongoose from 'mongoose';
+import { User } from 'next-auth';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../auth/[...nextauth]/options';
+import connectToDatabase from '@/app/lib/connectToDatabase';
+
+export async function GET(request: Request) {
+     await connectToDatabase();
+
+     const session = await getServerSession(authOptions);
+
+     const _user: User = session?.user;
+
+     if (!session || !_user) return Response.json({ success: false, message: 'Not authenticated' }, { status: 401 });
+
+     const userId = new mongoose.Types.ObjectId(_user._id);
+
+     try {
+          const user = await UserModel.aggregate([
+               // Stage 1: Match documents where _id is equal to the given userId
+               { $match: { _id: userId } },
+
+               // Stage 2: Deconstruct the 'messages' array field from the input documents to output a document for each element.
+               { $unwind: '$messages' },
+
+               // Stage 3: Sort the resulting documents by 'messages.createdAt' in descending order.
+               { $sort: { 'messages.createdAt': -1 } },
+
+               // Stage 4: Group the documents back into a single document per user, with an array 
+               // of all 'messages', now sorted by 'createdAt'.
+               { $group: { _id: '$_id', messages: { $push: '$messages' } } },
+          ])
+               .exec(); // Execute the aggregation pipeline and return the result           
+
+          if (!user || user.length === 0) return Response.json({ message: 'User not found', success: false }, { status: 404 });
+
+          return Response.json({ messages: user[0].messages }, { status: 200 });
+     }
+
+     catch (error: any) {
+          console.error('An unexpected error occurred: ', error);
+          return Response.json({ message: 'Internal server error', success: false }, { status: 500 });
+     }
+}
