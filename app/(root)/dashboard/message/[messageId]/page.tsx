d2b1683745e5c5f26c4fe8254b755700406bd728
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-08-12 17:07:25 +0530

    add image generation api. minor changes.

--- app/(root)/dashboard/message/[messageId]/page.tsx
+++ app/(root)/dashboard/message/[messageId]/page.tsx
@@ -10,65 +10,109 @@ import { Button } from '@/components/ui/button';
 import { MessageData } from '@/app/lib/models/message.schema';
 import { Message } from '@/app/lib/models/message.schema';
 import dayjs from 'dayjs';
+import puppeteer from 'puppeteer'
+import fs from 'fs';
+import { randomInt } from 'crypto';
 
 export default function MessagePage() {
-     const [messages, setMessages] = useState<MessageData[] | Message[] | any>([]);
+    const [messages, setMessages] = useState<MessageData[] | Message[] | any>([]);
+    const [imageSourceUrl, setImageSourceUrl] = useState("");
 
-     const params = useParams<{ messageId: string }>();
-     const messageId = params.messageId;
+    const params = useParams<{ messageId: string }>();
+    const messageId = params.messageId;
 
-     const { toast } = useToast();
+    const { toast } = useToast();
 
-     const { data: session } = useSession();
+    const { data: session } = useSession();
 
-     const fetchMessages = useCallback(async (refresh: boolean = false) => {
-          try {
-               const response = await axios.get<apiResponse>('/api/get-single-message?messageId=' + messageId);
-               setMessages(response.data.messages || []);
+    const fetchMessages = useCallback(async (refresh: boolean = false) => {
+        try {
+            const response = await axios.get<apiResponse>('/api/get-single-message?messageId=' + messageId);
+            setMessages(response.data.messages || []);
 
-               if (refresh) toast({
-                    title: 'Refreshed Messages',
-                    description: 'Showing latest messages',
-               });
-          }
+            if (refresh) toast({
+                title: 'Refreshed Messages',
+                description: 'Showing latest messages',
+            });
+        }
 
-          catch (error) {
-               const axiosError = error as AxiosError<apiResponse>;
-               toast({
-                    title: 'Error',
-                    description: axiosError.response?.data.message ?? 'Failed to fetch messages',
-                    variant: 'destructive',
-               });
-          }
-     }, [setMessages, toast, messageId]);
+        catch (error) {
+            const axiosError = error as AxiosError<apiResponse>;
+            toast({
+                title: 'Error',
+                description: axiosError.response?.data.message ?? 'Failed to fetch messages',
+                variant: 'destructive',
+            });
+        }
+    }, [setMessages, toast, messageId]);
 
-     useEffect(() => {
-          if (!session || !session.user) return;
+    useEffect(() => {
+        if (!session || !session.user) return;
 
-          fetchMessages();
-     }, [session, toast, fetchMessages]);
+        fetchMessages();
+    }, [session, toast, fetchMessages]);
 
-     const message = messages[0];
+    const message = messages[0];
 
-     return (
-          <div className="flex flex-col min-h-screen">
-               <div className="flex flex-col items-center justify-center flex-1 px-6 py-8 relative overflow-hidden">
-                    <div className="absolute top-0 left-0 w-full h-full animate-blob-1">
-                         <div className="absolute w-[400px] h-[400px] rounded-full bg-gradient-to-r from-[#96ffbf] to-[#ff7575] opacity-50 blur-3xl animate-blob-1" />
-                    </div>
-                    <div className="absolute top-0 right-0 w-full h-full animate-blob-2">
-                         <div className="absolute w-[300px] h-[300px] rounded-full bg-gradient-to-r from-[#373aff] to-[#e74c3c] opacity-50 blur-3xl animate-blob-2" />
-                    </div>
-                    <div className="absolute bottom-0 left-0 w-full h-full animate-blob-3">
-                         <div className="absolute w-[350px] h-[350px] rounded-full bg-gradient-to-r from-[#2980b9] to-[#ff6cbf] opacity-50 blur-3xl animate-blob-3" />
-                    </div>
-                    <h1 className="text-4xl md:text-6xl font-bold z-10 text-center">{message?.messages.content}</h1>
-                    <p className="text-muted-foreground mt-2 z-10">Sent at: {dayjs(message?.messages.createdAt).format('MMM D, YYYY h:mm A')}</p>
-                    <div className="mt-6 space-x-4 z-10 grid-cols-2">
-                         <Button>Add To Story</Button>
-                         <Button variant="secondary">Archive</Button>
-                    </div>
-               </div>
-          </div>
-     )
+    async function fetchBlob(url: string) {
+        const response = await fetch(url);
+        return response.blob();
+    }
+
+    // const imageSourceUrlNew = "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/e7acd37725c6ccb9f2cd03a4fbacfe15?_a=AQAEuiZ";
+
+    const imageSourceUrl1 = message
+        ? `https://tbh-tobehonest.vercel.app/api/og?question=${encodeURIComponent(message.messages.content)}`
+        : '';
+
+    const addToStory = async () => {
+        downloadImageAndSetSource();
+
+        console.log(`clicked shareImageAsset: ${imageSourceUrl1}`)
+        const fetchedImage = await fetch(imageSourceUrl1)
+        const blobImage = await fetchedImage.blob()
+        const fileName = imageSourceUrl1.split('/').pop()
+        const filesArray = [
+            new File([blobImage], (fileName as string), {
+                type: 'blob.type',
+                lastModified: Date.now(),
+            }),
+        ]
+        const shareData = {
+            title: fileName,
+            files: filesArray,
+            url: document.location.origin,
+        }
+        console.log(shareData)
+        if (navigator.canShare && navigator.canShare(shareData)) {
+            await navigator.share(shareData)
+        }
+    }
+
+    const downloadImageAndSetSource = async () => {
+        const image = await fetchBlob('/api/og?question=' + message.messages.content);
+        setImageSourceUrl(URL.createObjectURL(image));
+    }
+
+    return (
+        <div className="flex flex-col min-h-screen w-full">
+            <div className="flex flex-col items-center justify-center flex-1 px-6 py-8 relative overflow-hidden">
+                <div className="absolute top-0 left-0 w-full h-full animate-blob-1">
+                    <div className="absolute w-[400px] h-[400px] rounded-full bg-gradient-to-r from-[#96ffbf] to-[#ff7575] opacity-50 blur-3xl animate-blob-1" />
+                </div>
+                <div className="absolute top-0 right-0 w-full h-full animate-blob-2">
+                    <div className="absolute w-[300px] h-[300px] rounded-full bg-gradient-to-r from-[#373aff] to-[#e74c3c] opacity-50 blur-3xl animate-blob-2" />
+                </div>
+                <div className="absolute bottom-0 left-0 w-full h-full animate-blob-3">
+                    <div className="absolute w-[350px] h-[350px] rounded-full bg-gradient-to-r from-[#2980b9] to-[#ff6cbf] opacity-50 blur-3xl animate-blob-3" />
+                </div>
+                <h1 className="text-4xl md:text-6xl font-bold z-10 text-center">{message?.messages.content}</h1>
+                <p className="text-muted-foreground mt-2 z-10">Sent at: {dayjs(message?.messages.createdAt).format('MMM D, YYYY h:mm A')}</p>
+                <div className="mt-6 space-x-4 z-10 grid-cols-2">
+                    <Button onClick={addToStory}>Add To Story</Button>
+                </div>
+                {imageSourceUrl && <img src={imageSourceUrl} width={500} height={500}></img>}
+            </div>
+        </div>
+    )
 }
---
+++ app/api/og/route.tsx
@@ -0,0 +1,51 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+
+    const question = searchParams.get('question');
+
+    try {
+        return new ImageResponse(
+            (
+                <div
+                    style={{
+                        height: '100%',
+                        width: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundImage: 'linear-gradient(to bottom, black, #0f172b)',
+                        textAlign: 'center',
+                    }}
+                >
+                    <div tw="flex flex-col w-full h-full pt-20 items-center">
+                        <div tw="rounded-lg bg-white text-black shadow-sm w-full max-w-md overflow-hidden flex flex-col border border-neutral-950" style={{
+                            border: '1px solid gray',
+                        }}>
+                            <div tw="flex bg-black text-white justify-center" style={{
+                                backgroundImage: 'linear-gradient(90deg, #2563eb, #7c3aed)',
+                            }}>
+                                <h2 tw="text-2xl">q&a</h2>
+                            </div>
+                            <div tw="flex p-5 justify-center text-2xl">
+                                <p>{question || "Error. Please try again."}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ),
+            {
+                width: 720,
+                height: 1480,
+            }
+        )
+    }
+
+    catch (error: any) {
+        console.log(error);
+    }
+}
