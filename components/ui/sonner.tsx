8ef45504ce1bebc5abb9f991495b4f49a14ec12f
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2025-08-09 22:08:55 +0530

    minor ui change

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -9,7 +9,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group bg-background"
+      className="toaster group"
       style={
         {
           "--normal-bg": "var(--popover)",
